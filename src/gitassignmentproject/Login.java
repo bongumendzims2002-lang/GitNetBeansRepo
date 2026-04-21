3323958c14e5f71df314fd69d4aca87b4740c9a8
Mr Bongumendzi Masinga <bongumendzims2002@gmail.com>
2026-04-21 15:39:16 +0200

    Add Login.java with login message on feature-login branch

---
+++ src/gitassignmentproject/Login.java
@@ -0,0 +1,7 @@
+package gitassignmentproject;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Welcome! You have successfully logged in.");
+    }
+}
