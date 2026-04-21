119f469268b20fea3c1b5a6ea458f0f6773c24b3
Mr Bongumendzi Masinga <bongumendzims2002@gmail.com>
2026-04-21 14:47:16 +0200

    Initial commit - Hello NetBeans message

---
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,7 @@
+package gitassignmentproject;
+
+public class GitAssignmentProject {
+    public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git");
+    }
+}
